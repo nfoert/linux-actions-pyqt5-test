da80963d7a5971bf5fa98f7adcc61681aa85e004
nfoert <nofoert@gmail.com>
2023-07-24 10:58:32 -0400

    And another commit...

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QLabel
 
 app = QApplication([])
-label = QLabel("Hello, Linux!\nThanks for packaging this, GitHub actions.\nI put it in the wrong spot so here's *ANOTHER* commit!")
+label = QLabel("Hello, Linux!\nThanks for packaging this, GitHub actions.\nAnd another...")
 label.show()
 app.exec()
