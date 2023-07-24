f999af2dd98883abd7729d4b520442f6f5186c85
nfoert <nofoert@gmail.com>
2023-07-24 11:11:45 -0400

    and again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QLabel
 
 app = QApplication([])
-label = QLabel("Hello, Linux!\nThanks for packaging this, GitHub actions.\nAnd another.....") # There's got to be a faster way to test this...
+label = QLabel("Hello, Linux!\nThanks for packaging this, GitHub actions.\nAnd another......") # There's got to be a faster way to test this...
 label.show()
 app.exec()
